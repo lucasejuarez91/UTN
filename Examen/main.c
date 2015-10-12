038dd650cc71180b2efeb093f98c73cae5341475
Lucas Juarez <lucasejuarez91@gmail.com>
2015-10-12 20:58:59 -0300

    Create main.c

---
+++ Examen/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lib.h"
+#include "types.h"
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	int i=0;	
+//	llamo a la funcion que contiene mi menu
+	int valorReturn = menu(i);
+}
