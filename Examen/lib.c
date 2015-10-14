db66491f44b7a3c0c19ac80def55e50ab6d08d4b
Lucas Juarez <lucasejuarez91@gmail.com>
2015-10-14 16:20:34 -0300

    Update lib.c

--- Examen/lib.c
+++ Examen/lib.c
@@ -13,6 +13,7 @@ struct proveedor prov[3];
 int menu(int i){
 	int option;
 	int auxId;
+	
 	do{
 	printf("1.Alta\n2.Modificar\n3.Baja\n4.Informar\n5.Listar\n6.Salir\n>");
 	scanf("%d",&option);
@@ -21,43 +22,69 @@ int menu(int i){
 		case 1: // alta
 				printf("--Alta de Producto\n");
 					printf( "Codigo de Producto: 00%d\n",i );
-					controlCodigo(i);
+				
+					if (controlCodigo(i) == -1){
+						return -1;
+					}
 					prod[i].codProv = Proveedores(); // funcion que muestra los proveedores activos
+					
 					printf( "Descripcion: " );
 					scanf("%s",&prod[i].descrProd);
-					printf( "Importe: " );
-					validaInt(prod[i].impProd);
-					scanf("%d",&prod[i].impProd );
-					printf( "Cantidad: " );
-					scanf("%d",&prod[i].cantProd );
+					
+					do{
+						printf( "Ingrese importe: " );
+						scanf("%d",&prod[i].impProd );
+					} while (!validaInt(prod[i].impProd));
+					
+					do{
+						printf( "Ingrese cantidad: " );
+						scanf("%d",&prod[i].cantProd );
+					} while (!validaInt(prod[i].cantProd));
+					
 					i++;
+					
 				break;
 		case 2: //modificar
+				if(productosExistentes(i) == 0){
+					break;
+				} else {
 				printf("--Modificacion de Producto\n");
 				printf("Ingrese el numero de producto a modificar");
 				scanf("%d",&auxId);
-					for(j=0;j<5;j++) {
+					for(j=0;j<i;j++) {
 						if(prod[j].codProd==auxId) {
 								printf( "Codigo de Producto:%d\n",j);
-								printf( "Codigo Proveedor: " );
-								scanf("%d",&prod[j].codProv);
+								
+								prod[i].codProv = Proveedores();
+								
 								printf( "Descripcion: " );
 								scanf("%s",&prod[j].descrProd);
-								printf( "Importe: " );
-								scanf("%d",&prod[j].impProd );
-								printf( "Cantidad: " );
-								scanf("%d",&prod[j].cantProd );
+								
+								do{
+									printf( "Ingrese importe: " );
+									scanf("%d",&prod[i].impProd );
+								} while (!validaInt(prod[i].impProd));
+								
+								do{
+									printf( "Ingrese cantidad: " );
+									scanf("%d",&prod[i].cantProd );
+								} while (!validaInt(prod[i].cantProd));
+								
 								break;
 						}else{
 							printf("No existe el codigo a modificar.\n");
 							break;
 						}
 				}
+			}
 		case 3: //baja
+				if(productosExistentes(i) == 0){
+					break;
+				} else {
 				printf("--Baja de Producto\n");
 				printf("Ingrese el numero de producto a eliminar");
 				scanf("%d",&auxId);
-					for(j=0;j<5;j++) {
+					for(j=0;j<i;j++) {
 						if(prod[j].codProd==auxId) {
 								prod[j].codProd = -1;
 								prod[j].codProv = -1;
@@ -74,7 +101,11 @@ int menu(int i){
 						}
 				}
 				break;
+			}
 		case 4: //informar
+				if(productosExistentes(i) == 0){
+					break;
+				} else {
 				printf("--Informacion\n");
 					for(j=0;j<5;j++) {
 						if(j==0){
@@ -89,17 +120,21 @@ int menu(int i){
 						}
 					}
 				printf("Datos minimos\n");
-					for( j=0; j<5; j++ ) {
+					for( j=0; j<i; j++ ) {
 						printf("CodProd\tCodProv\tDescr\tImpor\tCant\n");
 						printf("%d\t%d\t%s\t%d\t%d\n",min[j].codProd,min[j].codProv,min[j].descrProd,min[j].impProd,min[j].cantProd);
 					}				
 				printf("Datos maximos\n");
-					for( j=0; j<5; j++ ) {
+					for( j=0; j<i; j++ ) {
 						printf("CodProd\tCodProv\tDescr\tImpor\tCant\n");
 						printf("%d\t%d\t%s\t%d\t%d\n",max[j].codProd,max[j].codProv,max[j].descrProd,max[j].impProd,max[j].cantProd);
 					}				
 				break;
+			}
 		case 5: //listar
+				if(productosExistentes(i) == 0){
+					break;
+				} else {
 				printf("--Listado de Productos\n");
 				for(i=0;i<5-1;i++) {
 					for(j=i+1;j<5;j++) {
@@ -117,11 +152,12 @@ int menu(int i){
 						}
 					}
 				}
-					for( j=0; j<5; j++ ) {
+					for( j=0; j<i; j++ ) {
 						printf("CodProd\tCodProv\tDescr\tImpor\tCant\n");
 						printf("%d\t%d\t%s\t%d\t%d\n",prod[j].codProd,prod[j].codProv,prod[j].descrProd,prod[j].impProd,prod[j].cantProd);
 					}
 				break;
+			}
 		case 6: // salir
 				printf("Gracias, vuelva pronto.");
 				break;
@@ -134,43 +170,42 @@ int menu(int i){
 
 int validaInt(int v){
 	if(v<0){
-		printf("Importe incorrecto");
+		printf("El valor debe ser mayor a cero!");
+		return 0;
 	}
+	
+	return 1;
 }
 
-int controlCodigo(i){
-	for(j=0;j=i;j++) {
-		if(prod[j].codProd==prod[i].codProd){
+int controlCodigo(int i){
+	for(j=0;j<i;j++) {
+		if(prod[j].codProd==i){
 			printf("Codigo ya utilizado.");
-		} else {
-			prod[i].codProd = i;
-		}
+			return -1;
+		} 
 	}
+	
+	prod[i].codProd = i;
+	return i;
 }
 
 int Proveedores(){
-		printf("Elija una opcion.\n")
+	prov[0].descrProv = "Manaos";
+	prov[1].descrProv = "Pepsi";
+	prov[2].descrProv = "Kodak";
+		printf("Elija una opcion.\n");
 		printf( "Proveedor 1: %s\nProveedor 2: %s\n Proveedor 3: %s",prov[0].descrProv,prov[1].descrProv,prov[2].descrProv);
+	
+	int codProv=0;
+	scanf("%i",&codProv );
+	
+	return codProv;
 }
 
-int validaCantidad(int c){
-	if(c<0){
-		printf("Cantidad incorrecta.");
-	} else {
-		scanf("%d",&prod[i].cantProd );
-	}
-}
-
-int productosExistentes(){
-	if(prod[0].codProd==0){
+int productosExistentes(int i){
+	if(i==0){
 		printf("Para realizar otras operaciones primero debe ingresar un producto.");
+		return 0;
 	}
-}
-
-int cantAgregada(){
-	for(j=0;j=i;j++) {
-		if(prod[j].codProd!=0){
-			acum++;
-		}
-	}
+	return 1;
 }
